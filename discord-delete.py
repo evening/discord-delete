a5b908a41a4c324bb57ded6273ad22369d45215b
evening <39542044+evening@users.noreply.github.com>
2020-10-22 21:58:45 -0400

    add progress bar

--- discord-delete.py
+++ discord-delete.py
@@ -3,12 +3,16 @@ import requests
 import sys
 import time
 
+# from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import (confirm, ProgressBar)
+from prompt_toolkit.styles import Style
+
 config = configparser.ConfigParser()
 config.read("account.ini")
 s = requests.Session()
 s.headers, base = (
     {"Authorization": config["Settings"]["auth_key"]},
-    "https://discordapp.com/api/v6",
+    "https://discordapp.com/api/v8",
 )
 
 
@@ -21,11 +25,9 @@ def convert_dm_rid(uid):
 
 
 def main():
-    user_id = s.get(f"{base}/users/@me").json()["id"]
-    resource_id = config["Settings"]["resource_id"]
-
     params = {k: v for k, v in config["Params"].items() if v}
-    params.update({"author_id": user_id})
+    params.update({"author_id": s.get(f"{base}/users/@me").json()["id"]})
+    resource_id = config["Settings"]["resource_id"]
 
     if config["Settings"]["type"] in ("server", "guilds"):
         resource_type = "guilds"
@@ -34,19 +36,26 @@ def main():
         resource_type = "channels"
     else:
         sys.exit("set `type` to either `server` or `DM`")
-        
-    info(resource_type, resource_id)
-    for c, msg in enumerate(messages(resource_type, resource_id, params), 1):
-        # https://discord.com/developers/docs/reference#snowflakes
-        d = time.gmtime(((int(msg["id"]) >> 22) + 1_420_070_400_000) / 1000)
-        print(c, f"{d.tm_mon}/{d.tm_mday}/{d.tm_year}", repr(msg["content"]))
-        delete(msg)
+
+    to_delete = f"messages in {get_title(resource_type, resource_id)}"
+    if not confirm(f"Delete {to_delete}?"):
+        sys.exit()
+    with ProgressBar(
+        title=f"Deleting {to_delete}",
+        style=Style.from_dict({"bottom-toolbar": "noreverse"}),
+    ) as pb:
+        num_messages = s.get(
+            f"{base}/{resource_type}/{resource_id}/messages/search", params=params
+        ).json()["total_results"]
+        for msg, p in zip(
+            messages(resource_type, resource_id, params), pb(range(num_messages)),
+        ):
+            delete(msg, pb)
 
 
 def messages(res, rid, params):
     r = s.get(f"{base}/{res}/{rid}/messages/search", params=params)
-    code = r.status_code
-    if code == 200:
+    if r.status_code == 200:
         msgs = r.json()["messages"]
         recovered = False
         for msg in msgs:
@@ -57,34 +66,37 @@ def messages(res, rid, params):
             del msg
         if recovered:
             yield from messages(res, rid, params)
-    elif code == 429:
+    elif r.status_code == 429:
         t = r.json()["retry_after"]
-        print("Limited", code, t)
-        time.sleep(t / 950)
-    elif code >= 400:
-        print(f"Error", code, r.text)
+        time.sleep(t)
+        messages(res, rid, params)
+    elif r.status_code >= 400:
+        sys.exit(r.status_code, r.json())
 
 
-def delete(msg):
+def delete(msg, pb):
+    # https://discord.com/developers/docs/reference#snowflakes
+    d = time.gmtime(((int(msg["id"]) >> 22) + 1_420_070_400_000) / 1000)
     r = s.delete(f"{base}/channels/{msg['channel_id']}/messages/{msg['id']}")
-    code = r.status_code
-    if code == 429:
+    if r.status_code == 429:
         t = r.json()["retry_after"]
-        print("Limited", code, t)
-        time.sleep(t / 950)
-    elif code >= 400:
-        print("Error", code, r.text)
+        pb.bottom_toolbar = f"Limited: {t}"
+        time.sleep(t)
+        delete(msg, pb)
+    elif r.status_code >= 400:
+        sys.exit(r.status_code, r.json())
     else:
+        pb.bottom_toolbar = f"{d.tm_mon}/{d.tm_mday}/{d.tm_year} {msg['content']}"
         time.sleep(0.15)
 
 
-def info(res, sid):
+def get_title(res, sid):
     username = s.get(f"{base}/users/@me").json()["username"]
     r = s.get(f"{base}/{res}/{sid}").json()
     if res == "guilds":
-        print(f"Deleting messages in {r['name']} by {username}")
+        return f"{r['name']} by {username}"
     elif res == "channels":
-        print(f"Deleting messages with {r['recipients'][0]['username']} by {username}")
+        return f"{r['recipients'][0]['username']} by {username}"
 
 
 if __name__ == "__main__":
